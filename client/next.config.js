e1d7656f935a8b40f06443604009f94ea07255c9
Oliver Clennan <oclennan@uwo.ca>
2024-11-03 11:02:02 -0700

    Minor configuration changes.

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     webpack: (config) => {
         config.watchOptions.poll = 300;
-        config.infrastructureLogging = { debug: /PackFileCache/ }
         return config;
     },
 };
