cb0df09480300eff95aff535db1afe3248ad5b97
Oliver Clennan <oclennan@uwo.ca>
2025-02-18 17:56:43 -0700

    Minor UI changes.

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header({ currentUser }) {
         });
 
     return (
-        <div className="text-blue-xlight text-lg flex justify-between px-6 items-center h-[10svh]">
+        <div className="text-blue-xlight text-lg flex justify-between px-6 items-center h-[10vh]">
             <Link href="/" className="font-title text-2xl">
                 Ticket-Exchange
             </Link>
