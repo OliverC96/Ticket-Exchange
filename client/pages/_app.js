872bd09b727e9fa0394c16bee160ac1552059420
Oliver Clennan <oclennan@uwo.ca>
2025-02-27 15:38:19 -0700

    Minor UI changes.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -39,7 +39,7 @@ export default function AppComponent({Component, pageProps, currentUser}) {
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-                <meta name="theme-color" content="var(--bg)" />
+                <meta name="theme-color" content="#003049" />
                 <title> Ticket Exchange </title>
             </Head>
             <PostHogProvider client={posthog}>
